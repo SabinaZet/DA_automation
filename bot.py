347450f56979971b5aa2640d5f4339343dd68251
SabinaZet <104944637+SabinaZet@users.noreply.github.com>
2026-02-24 13:23:07 +0100

    Add files bot.py and requirements.txt

---
+++ bot.py
@@ -0,0 +1,91 @@
+import requests
+import os
+import json
+from bs4 import BeautifulSoup
+
+cookies = os.environ['cookies']
+headers = os.environ['headers']
+url = 'https://app.dataannotation.tech/workers/projects'
+telegram = os.environ['TELEGRAM_TOKEN']
+chat = os.environ['CHAT_ID']
+path = 'IDS.txt'
+
+def get_response():
+    response = requests.get(url, cookies=cookies, headers=headers)
+    print(f'Response status code: {response.status_code}')
+    
+    return response
+
+def parse_html(response):
+    soup = BeautifulSoup(response.text, 'html.parser')
+    div = soup.find('div', {'id':'workers/WorkerProjectsTable-hybrid-root'})
+    print(f'Data div found: {div is not None}')
+    data_props = div['data-props']
+    data = json.loads(data_props)
+    projects = data['dashboardMerchTargeting']['projects']
+    
+    return projects
+
+def get_ids(projects):
+    ids = [i['id'] for i in projects]
+    
+    return ids
+
+def save_ids(ids):
+  sids = '\n'.join(ids)
+  with open(path, 'w') as f:
+    f.write(sids)
+    print('IDs updated!')
+
+  f.close()
+  
+def send_telegram_message(text):
+    url = f"https://api.telegram.org/bot{telegram}/sendMessage"
+    
+    payload = {
+        "chat_id": chat,
+        "text": text
+    }
+    
+    response = requests.post(url, data=payload)
+    print("Telegram status:", response.status_code)
+
+def check_updates(ids):
+  try:
+    with open(path, 'r') as f:
+      old = set([id.strip() for id in f.readlines()])
+      new = set(ids)
+      n = len(new - old)
+
+      if n > 0:
+        send_telegram_message(f"You've got {n} new projects!")
+        save_ids(ids)
+
+      else:
+        print('Nothing new :(')
+
+      if len(new) < len(old):
+        print('Less IDs than before.')
+        save_ids(ids)
+
+      f.close()
+    
+  except:
+    print(f'Wrong path: {path}')
+    send_telegram_message(f'Error with file {path}!')
+
+def main():
+    response = get_response()
+    projects = parse_html(response)
+    ids = get_ids(projects)
+    try:
+        if os.path.exists(path):
+            check_updates(ids)
+        elif not os.path.exists(path):
+            save_ids(ids)
+            check_updates(ids)
+    except:
+        print('Error on main!')
+        
+if __name__ == "__main__":
+    main()
