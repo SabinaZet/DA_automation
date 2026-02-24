8bcaf4c69bc82ec8dc5c752b67538f28306f00cc
SabinaZet <104944637+SabinaZet@users.noreply.github.com>
2026-02-24 14:27:34 +0100

    Pull headers into code bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,28 @@ import json
 from bs4 import BeautifulSoup
 
 cookies = os.environ['cookies']
-headers = os.environ['headers']
 url = 'https://app.dataannotation.tech/workers/projects'
 telegram = os.environ['TELEGRAM_TOKEN']
 chat = os.environ['CHAT_ID']
 path = 'IDS.txt'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'pl,en-US;q=0.9,en;q=0.8',
+    # 'Accept-Encoding': 'gzip, deflate, br, zstd',
+    'Referer': 'https://accounts.google.com/',
+    'Alt-Used': 'app.dataannotation.tech',
+    'Connection': 'keep-alive',
+    # 'Cookie': 'conv_session=eyJpZCI6ImZiMTZmZWE4OTI5OTZkMDI1MTUxMjdlNTkwZDZhZWVkYTUwNThkZGVmZGVjM2RlODM3N2E1YTYxZWM2ODY3M2NmNjVhMGU3ODNhZDAxZDkyMDYxNTBjNDcxNiIsImxpbmtpbmdfcmVxdWlyZWQiOmZhbHNlfQ%3D%3D; gondor-main=a3a300b1ff8e92d7510cdfa953d40158; AMP_50c088a20a=JTdCJTIyZGV2aWNlSWQlMjIlM0ElMjIyMjE3MWZmNS04M2UwLTQwZjgtOGEzZS1iMmRkZjUyZWU1ZjQlMjIlMkMlMjJ1c2VySWQlMjIlM0ElMjI1YTI2NWY5YS1mYTg5LTQyOTktOTcwMy0yNWE3ZDAzZjdmNTYlMjIlMkMlMjJzZXNzaW9uSWQlMjIlM0ExNzcxOTA4ODg5ODc4JTJDJTIyb3B0T3V0JTIyJTNBZmFsc2UlMkMlMjJsYXN0RXZlbnRUaW1lJTIyJTNBMTc3MTkwOTk5ODg2MCUyQyUyMmxhc3RFdmVudElkJTIyJTNBMTklMkMlMjJwYWdlQ291bnRlciUyMiUzQTUlN0Q=; AMP_MKTG_50c088a20a=JTdCJTIycmVmZXJyZXIlMjIlM0ElMjJodHRwcyUzQSUyRiUyRmFjY291bnRzLmdvb2dsZS5jb20lMkYlMjIlMkMlMjJyZWZlcnJpbmdfZG9tYWluJTIyJTNBJTIyYWNjb3VudHMuZ29vZ2xlLmNvbSUyMiU3RA==',
+    'Upgrade-Insecure-Requests': '1',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'cross-site',
+    'Sec-Fetch-User': '?1',
+    'DNT': '1',
+    'Sec-GPC': '1',
+    'Priority': 'u=0, i'
+}
 
 def get_response():
     response = requests.get(url, cookies=cookies, headers=headers)
